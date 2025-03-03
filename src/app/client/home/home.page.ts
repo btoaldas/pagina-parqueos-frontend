f90beded7dff9e86fc770dd33d45ed5e2034baaf
Alberto Aldas <albertoalex@gmail.com>
2025-03-02 23:22:01 -0500

    Menu Design

--- src/app/client/home/home.page.ts
+++ src/app/client/home/home.page.ts
@@ -6,13 +6,19 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonText,
+  IonIcon,
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { people, cashOutline, carOutline, warning } from 'ionicons/icons';
 
 @Component({
-  selector: 'app-home',
+  selector: 'page-home',
   templateUrl: './home.page.html',
   standalone: true,
   imports: [
+    IonIcon,
+    IonText,
     IonContent,
     IonHeader,
     IonTitle,
@@ -22,7 +28,21 @@ import {
   ],
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  constructor() {
+    addIcons({ people, cashOutline, carOutline, warning });
+  }
+
+  getDate(): string {
+    const fecha = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    };
+
+    return fecha.toLocaleDateString('es-ES', options);
+  }
 
   ngOnInit() {}
 }
