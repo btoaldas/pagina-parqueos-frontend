56b2de4d2fc2239a5e796ed4d6cfd2663b8170cb
Alberto Aldas <albertoalex@gmail.com>
2025-03-07 15:33:01 -0500

    Operator 10%

--- src/app/employ/operator/operator.page.ts
+++ src/app/employ/operator/operator.page.ts
@@ -1,20 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonIcon,
+  IonButton,
+  IonText,
+  IonSearchbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircleOutline, peopleOutline } from 'ionicons/icons';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-operator',
   templateUrl: './operator.page.html',
-  styleUrls: ['./operator.page.scss'],
+  styles: [
+    `
+      ion-segment-button::part(indicator-background) {
+        background: rgb(107 114 128);
+      }
+    `,
+  ],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonSearchbar,
+    IonText,
+    IonButton,
+    IonIcon,
+    IonLabel,
+    IonSegmentButton,
+    IonSegment,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class OperatorPage implements OnInit {
+  ticketSubject = new Subject<string>();
+  selectedTab: 'entrada' | 'salida' | 'multas' = 'entrada';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.ticketSubject.subscribe((searchTerm: string) => {
+      console.log({ searchTerm });
+    });
+    addIcons({ peopleOutline, addCircleOutline });
   }
 
+  onSearch(event: any): void {
+    const searchTerm = event.target.value;
+    this.ticketSubject.next(searchTerm);
+  }
+
+  ngOnInit() {}
 }
