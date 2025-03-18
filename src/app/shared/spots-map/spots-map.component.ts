2383e91516d7aa0bdfaca4995728eaa108a0c419
Cristian Riofrio <cristianriofrio@hotmail.com>
2025-03-18 12:12:42 -0500

    Add custom icons for available and unavailable parking spots on the map

--- src/app/shared/spots-map/spots-map.component.ts
+++ src/app/shared/spots-map/spots-map.component.ts
@@ -1,6 +1,7 @@
 import { SpaceResponse } from '@/app/models/space.model';
 import { SpaceService } from '@/app/services/space.service';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import * as L from 'leaflet';
 
 @Component({
@@ -19,35 +20,48 @@ export class SpotsMapComponent implements OnInit {
   private map: L.Map | null = null;
   private spaces: SpaceResponse[] = [];
 
-  constructor(private spaceService: SpaceService) {}
+  private iconLocationRed: string;
+  private iconLocationGreen: string;
 
-  // Configuración e inicialización del mapa
+  constructor(
+    private spaceService: SpaceService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconLocationRed = 'assets/icon/location.red.svg';
+    this.iconLocationGreen = 'assets/icon/location.green.svg';
+  }
+
   private initMap(): void {
-    this.map = L.map('map').setView([20, 0], 2); // Configura el centro del mapa y el zoom inicial
+    this.map = L.map('map').setView([20, 0], 2);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
 
-    // Renderizar spots en el mapa
-
-    this.spaces.forEach((space) => {
-      const marker = L.marker([space.latitude, space.longitude])
-        .addTo(this.map!)
-        .bindPopup(`<b>${space.id}</b><br>Status: ${space.state}`) // Mostrar popup con información
-        .on('click', () => this.openGoogleMaps(space)); // Abrir Google Maps al hacer clic
+    const markerGreenIcon = L.icon({
+      iconUrl: this.iconLocationGreen,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+    });
+    const markerRedIcon = L.icon({
+      iconUrl: this.iconLocationRed,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
     });
 
-    /* this.spots.forEach(spot => {
-      const marker = L.marker([spot.latitude, spot.longitude])
-        .addTo(this.map)
-        .bindPopup(`<b>${spot.name}</b><br>Status: ${spot.status}`) // Mostrar popup con información
-        .on('click', () => this.openGoogleMaps(spot)); // Abrir Google Maps al hacer clic
-    }); */
+    this.spaces.forEach((space) => {
+      const marker = L.marker([space.latitude, space.longitude], {
+        icon: space.state === 'disponible' ? markerGreenIcon : markerRedIcon,
+      })
+        .addTo(this.map!)
+        .bindPopup(`<b>${space.id}</b><br>Status: ${space.state}`)
+        .on('click', () => this.openGoogleMaps(space));
+    });
   }
 
-  // Abrir Google Maps al hacer clic en un spot
   openGoogleMaps(spot: SpaceResponse) {
     const url = `https://www.google.com/maps?q=${spot.latitude},${spot.longitude}`;
     window.open(url, '_blank');
